711756c8df8d8815bbd2b4b776040bbc2434c32a
harshit <harshsha64@gmail.com>
2024-02-17 19:15:13 +0000

    added audio i think

--- hypr/.config/hypr/startup.sh
+++ hypr/.config/hypr/startup.sh
@@ -2,3 +2,14 @@
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
+
+hyprpaper &
+
+wl-paste --type text --watch cliphist store & #Stores only text data
+
+wl-paste --type image --watch cliphist store & #Stores only image data 
+
+#mpvpaper -o "--loop" HDMI-A-1 $HOME/.config/wallpapers/neutron-star-desktop-wallpaperwaifu-com.mp4 &
+#mpvpaper -o "--loop" eDP-1 $HOME/.config/wallpapers/neutron-star-desktop-wallpaperwaifu-com.mp4 &
+
