135d2c0b33c5891cc8aedf2020e1b28a6088054b
harshit <harshsha64@gmail.com>
2024-04-16 19:04:43 +0530

    pretty

--- hypr/.config/hypr/scripts/temp_terminal.sh
+++ hypr/.config/hypr/scripts/temp_terminal.sh
@@ -4,5 +4,16 @@
 TIMEOUT_DURATION=7
 
 # Open Kitty terminal
-kitty --class floatingkitty zsh -c "echo 'Terminal will auto-close in $TIMEOUT_DURATION seconds'; (sleep $TIMEOUT_DURATION; pkill -P $$) & exec zsh"
+kitty --class floatingkitty zsh -c "
+echo 'Terminal will auto-close in $TIMEOUT_DURATION seconds';
+# Start a subshell to manage the timeout and process termination
+(
+    # Wait for the specified timeout duration
+    sleep $TIMEOUT_DURATION;
+    # Kill the parent process group, effectively closing the terminal
+    pkill -P $$
+) & 
+# Execute a new instance of Zsh
+exec zsh"
+
 
